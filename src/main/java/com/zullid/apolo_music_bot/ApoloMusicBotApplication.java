32d19fc45981829d5eb0064c2c2ffd70bf9265d0
Dante Zulli <dantezulli2004@gmail.com>
2025-06-04 21:11:09 -0300

    initial commit

---
+++ src/main/java/com/zullid/apolo_music_bot/ApoloMusicBotApplication.java
@@ -0,0 +1,13 @@
+package com.zullid.apolo_music_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoloMusicBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoloMusicBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zullid/apolo_music_bot/ApoloMusicBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zullid.apolo_music_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoloMusicBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
